decc35893fcfca6283e98c940008cff16f53b9bf
mohammedramadan99 <lifeisagame0101@gmail.com>
2023-10-07 01:49:53 +0300

    feat: Update toolbar component

--- client/src/components/Layout/Toolbar/Toolbar.jsx
+++ client/src/components/Layout/Toolbar/Toolbar.jsx
@@ -1,10 +1,13 @@
 import bell from "@assets/icons/1/bell.png";
 import { Menu } from "@mui/icons-material";
+import { Avatar, useTheme } from "@mui/material";
 
 function Toolbar({ setOpenSidebar }) {
+  const theme = useTheme()
   const newNotification = true;
 
   const user = {
+    name:"Mohammed",
     username: "محمد رمضان",
     avatar:""
   }
@@ -17,12 +20,21 @@ function Toolbar({ setOpenSidebar }) {
       <div className="topNavbar-left">
         <div
           className={`topNavbar-bell ${newNotification ? "green-square" : ""}`}>
-          <img src={bell} alt="bell" />
+          {/* <img src={bell} alt="bell" /> */}
         </div>
         <div className="topNavbar-user">
           <div className="topNavbar-user-name">{user.username}</div>
           <div className="topNavbar-user-img">
-            <img src={user.avatar} alt="avatar" />
+          <Avatar
+              sx={{
+                bgcolor: theme.palette.role.superadmin,
+                width: 40,
+                height: 40,
+                fontSize:15
+              }}
+            >
+              {user.name.slice(0, 2)}
+            </Avatar>
           </div>
         </div>
       </div>
