f92491c8cd4eaac2c1b814a9e867dad9763dddc8
mohammedramadan99 <lifeisagame0101@gmail.com>
2023-10-24 02:47:00 +0300

    fix: add folder

--- client/src/components/Folders/FoldersDialog/FoldersDialog.jsx
+++ client/src/components/Folders/FoldersDialog/FoldersDialog.jsx
@@ -85,7 +85,7 @@ export default function FoldersDialog({ open, setOpen, footer }) {
       <Dialog
         fullScreen={fullScreen}
         fullWidth={true}
-        open={update}
+        open={update || add}
         onClose={handleClose}
         aria-labelledby="responsive-dialog-title">
         <DialogTitle id="responsive-dialog-title">
